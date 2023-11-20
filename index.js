9d4038534fa3c8f93c0afd696c7f5f244d76ac17
Vito Ghifari <vitosgh@gmail.com>
2023-11-20 11:24:21 +0700

    fix starting server

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import express from "express";
+import express from 'express';
+
+const PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
