fe880e453895ee9fa97ace98b9ab182a2cb39984
Vito Ghifari <vitosgh@gmail.com>
2023-11-21 11:58:51 +0700

    add login and register

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+
+dotenv.config();
 
 const PORT = 3000;
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,78 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import prisma from '../config/db.js';
+
+const saltRounds = 10;
+
+export const register = async (req, res) => {
+  const {
+    firstName, lastName, email, password,
+  } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      data: 'user created',
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid email or password',
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid email or password',
+      });
+    }
+
+    const token = await jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        token,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
