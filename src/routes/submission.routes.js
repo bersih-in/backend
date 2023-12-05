efd50d07247cecda662b3e10de25e477846426fb
Vito Ghifari <vitosgh@gmail.com>
2023-12-05 11:25:22 +0700

    add report by ID for user

--- src/routes/submission.routes.js
+++ src/routes/submission.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
 import authenticateUser from '../middlewares/auth.middleware.js';
-import { submit, getSubmissionsByUserId, getSubmissionsBySelf } from '../controllers/submission.controller.js';
+import { submit, getSubmissionsByUserId, getSubmissionsBySelf, getSubmissionById } from '../controllers/submission.controller.js';
 
 const router = express.Router();
 
 router.post('/submit', authenticateUser, submit);
 router.get('/user/:userId', getSubmissionsByUserId);
 router.get('/self', authenticateUser, getSubmissionsBySelf);
+router.get('/:id', authenticateUser, getSubmissionById);
 
 export default router;
